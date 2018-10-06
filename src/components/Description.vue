ecd2643d9efc9ca887e46f11f5fb6f7157e452b4
Eko Simanjuntak <ecojuntak@gmail.com>
2018-10-06 20:59:35 +0700

    update description

--- src/components/Description.vue
+++ src/components/Description.vue
@@ -15,7 +15,7 @@ export default {
       descriptions: [
         "<b> I'm student at Del Institute of Technology. Previously I work as Full Stack Software Engineer Intern at GO-JEK. </b>",
         "I am a curious learner specially about new technology. I spend my time to read articles and tutorials everyday. I also challenge my self to learn new stuff.",
-        "I love to write code. I write code for my study and my self improvement. I also write code and blog to share what I had learned. I love to connect to new people that I can work and learn with. If you love to share something to me, feel free to reach me."
+        "I love to write code. I write code for my study and my self improvement. I also write code and blog to share what I had learned. I love to connect to new people that I can work and learn with. If you love to share something to me, feel free to reach me and let have some coffee."
       ]
     };
   }
